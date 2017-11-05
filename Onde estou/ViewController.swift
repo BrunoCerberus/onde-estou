5b92bf6ce706df6a322d99271399cabcc84c75ee
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-05 00:39:00 -0200

    Algumas configuracoes a mais

--- Onde estou/ViewController.swift
+++ Onde estou/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
